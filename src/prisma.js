271d1b2eeb43d763f93963db0f0469c547bf9b75
Sikktna <Sikktna@gmail.com>
2025-09-23 00:05:59 +0300

    edit 1

--- src/prisma.js
+++ src/prisma.js
@@ -1,11 +1,17 @@
 // prisma.js
 import { PrismaClient } from '@prisma/client';
+import dotenv from 'dotenv'; 
+
+dotenv.config();
 
 const prisma = new PrismaClient({
   log: process.env.NODE_ENV === 'test' ? [] : ['query', 'info', 'warn', 'error'],
+  // --- START: ADD OR CORRECT THIS PART ---
   datasources: {
     db: {
-      url: process.env.NODE_ENV === 'test' ? process.env.DATABASE_URL_TEST : process.env.DATABASE_URL,
+      url: process.env.NODE_ENV === 'test' 
+        ? process.env.DATABASE_URL_TEST 
+        : process.env.DATABASE_URL,
     },
   },
 });
